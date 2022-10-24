8803cd4251a1898e2851a522539e29ec322dc951
AddyPrairie <addison.prairie@gmail.com>
2022-10-24 11:41:44 -0500

    fix syntax highlighting

--- wgsl.js
+++ wgsl.js
@@ -1,7 +1,7 @@
 CodeMirror.defineSimpleMode("wgsl", {
   start: [
     
-    {regex: /\b([a-zA-Z0-9_]+)(\s*\()/, token: [null, "function", null]},
+    {regex: /\b([a-zA-Z0-9_]+)(\s*\()/, token: ["function", null]},
     {regex: /\b(var|vec(2|3|4)|fn|mat[0-9]x[0-9]|f32|i32|u32|bool|struct)\b/, token: "type"},
     {regex: /(@)(location)(\()/, token: [null, "type", null]},
     {regex: /\b([0-9\.-]+)\b/, token:["literal"]},
